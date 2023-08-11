ac00e4f3e5ecb0181a529c5f5e2de002a2edab0e
Mai Congyi 麦从一 <microboym@icloud.com>
2023-08-11 20:12:39 +0800

    Update ddim.py: Fix the condition of devices.

--- audioldm2/latent_diffusion/models/ddim.py
+++ audioldm2/latent_diffusion/models/ddim.py
@@ -23,7 +23,7 @@ class DDIMSampler(object):
     def register_buffer(self, name, attr):
         if type(attr) == torch.Tensor:
             if attr.device != self.device:
-                if self.device == torch.device("mps") and attr.dtype==torch.float64:
+                if self.device == "mps" and attr.dtype==torch.float64:
                     attr = attr.to(self.device, dtype=torch.float32)
                 else:
                     attr = attr.to(self.device)
