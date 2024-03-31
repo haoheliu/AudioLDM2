b653bc11f4034a8d6c6d712b5f76f36268311a05
[nanana] <[100232@naver.com]>
2024-03-31 12:27:31 +0900

    Adding run file

--- audioldm2/run_cpu.py
+++ audioldm2/run_cpu.py
@@ -6,10 +6,10 @@ repo_id = "cvssp/audioldm2"
 pipe = AudioLDM2Pipeline.from_pretrained(repo_id, torch_dtype=torch.float)
 pipe = pipe.to("cpu")
 
-#text 프롬프트 
+#text prompt 
 prompt = "Techno music with a strong, upbeat tempo and high melodic riffs."
 audio = pipe(prompt, num_inference_steps=200, audio_length_in_s=15.0).audios[0]
 # audio_length_in_s = 생성 음악 시간.
 
-#파일 쓰기 + 음악 파일 제목 설정
-scipy.io.wavfile.write("techno_15.wav", rate=16000, data=audio)
+audiofile_name = "techno.wav"
+scipy.io.wavfile.write(audiofile_name, rate=16000, data=audio)
